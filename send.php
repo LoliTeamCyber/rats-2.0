395cfdf4c1971265df0b9e171d85030a6fa6d1a8
LoliTeamCyber <101130397+LoliTeamCyber@users.noreply.github.com>
2022-07-06 23:13:29 +0700

    Add files via upload

---
+++ send.php
@@ -0,0 +1,21 @@
+<?php
+
+echo $banner = "
+
+
+\x1b[34m██████╗░░█████╗░████████╗
+██╔══██╗██╔══██╗╚══██╔══╝
+██████╔╝███████║░░░██║░░░
+██╔══██╗██╔══██║░░░██║░░░
+██║░░██║██║░░██║░░░██║░░░
+╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░ 2.0
+
+\x1b[31mYOUTUBE : ANROID SECURIRY
+\x1b[97mTWITTER : ANDROID SECURITY\n";
+
+#Sender rats
+
+#RatsCodes()
+
+#x5o!p%@ap[4\pzx54(p^)7cc)7}$eicar-standard-antivirus-test-file!$h+h*
+
